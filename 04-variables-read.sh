a3932c6c3dcd3595db5ae3eae90e1a6315ef47cd
Dasarivinay123 <dasariv211@gmail.com>
2026-05-21 15:23:56 +0530

    read command

---
+++ 04-variables-read.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Enter PERSON1 name:"
+read PERSON1
+
+echo "Enter PERSON2 name:"
+read PERSON2
+
+echo "$PERSON1: Hey $PERSON2, we are going blast you!"
+echo "$PERSON2: yes, please. you are welcome"
