efefe17dc997a4f395461226fdcd40fbca412037
Dasarivinay123 <dasariv211@gmail.com>
2026-05-22 13:06:32 +0530

    conditions

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -11,7 +11,7 @@ NUMBER=$1
 
 if [ $NUMBER -gt  20 ]; then
     echo "Given number is greater than  20"
-elif [ $NUMBER -eq 20 ]
+elif [ $NUMBER -eq 20 ]; then
     echo "Given number is equal to 20"
 else
     echo "Given number $NUMBER is less than 20"
