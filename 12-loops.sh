80183381dbef74c33b2976cfc32b42b5e52ac059
Dasarivinay123 <dasariv211@gmail.com>
2026-05-25 12:41:22 +0530

    update time_stamp in script

--- 12-loops.sh
+++ 12-loops.sh
@@ -3,6 +3,7 @@
 USERID=$(id -u)
 LOGS_DIR=/var/log/shell-script
 LOGS_FILE="$LOGS_DIR/$0.log"
+TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
 #check root access or not
 
 if [ $USERID -ne 0 ]; then
@@ -14,10 +15,10 @@ fi
 #second arg -> exit code
 VALIDATE(){
     if [ $2 -ne 0 ]; then
-        echo "Installing $1 is ...FAILED" | tee -a $LOGS_FILE
+        echo "$TIMESTAMP [ERROR] Installing $1 is ...FAILED" | tee -a $LOGS_FILE
         exit 1
     else 
-        echo "Installing $1 ....SUCCESS" | tee -a $LOGS_FILE
+        echo "$TIMESTAMP [INFO] Installing $1 ....SUCCESS" | tee -a $LOGS_FILE
     fi
 }
 for package in $@
